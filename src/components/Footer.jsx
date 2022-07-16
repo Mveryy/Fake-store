9f005c8a2e3d46e928c3534ead572e472b7ea8ab
Mvery <matheus.verissimo@outlook.com>
2022-07-16 01:44:00 -0300

    add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+  return (
+    <div
+      className="bg-[#9353c7] text-white w-full p-4 text-center flex flex-col gap-2 font-bold "
+      style={{ boxShadow: "0px - 1px 8px 0px rgb(0 0 0 / 0.2)" }}>
+      <p className="" >©2022{" "}
+        <a href="https://github.com/Mveryy" className="hover:underline">Matheus Verissimo</a>
+
+      </p>
+    </div >
+  )
+}
